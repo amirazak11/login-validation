d10bbca3e8f8b464e3d89fa6f9ede9d0565f39dc
amirazak11 <amirazak1112018@gmail.com>
2022-10-13 20:30:52 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,8 @@
+var usernameInput = document.getElementById("usernameInput"); 
+var userEmailInput = document.getElementById("userEmailInput"); 
+var userPasswordInput = document.getElementById("userPasswordInput"); 
+var signupBtn = document.getElementById("signupBtn");
+function signUp()
+{
+    
+}
